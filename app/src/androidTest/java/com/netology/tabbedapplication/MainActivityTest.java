eac0d9f2b6341b22ddf4caecdeeaa986805a8986
Neto test <dimmakarovdv89@mail.ru>
2026-03-15 22:59:06 +0500

    Initial commit: Android test project with failing Espresso test

---
+++ app/src/androidTest/java/com/netology/tabbedapplication/MainActivityTest.java
@@ -0,0 +1,31 @@
+package com.netology.tabbedapplication;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() throws Exception {
+        onView(withText("Page: 1")).check(matches(isDisplayed()));
+        onView(withText("TAB 2")).perform(click());
+        onView(withText("Page: 2")).check(matches(isDisplayed()));
+    }
+}
